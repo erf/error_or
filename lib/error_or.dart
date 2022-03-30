0818673503ce9a304b8ce443657ed6b7299b1ff4
Erlend Lind Madsen <erlendf80@gmail.com>
2022-03-31 00:10:54 +0200

    make default constructor private

--- lib/error_or.dart
+++ lib/error_or.dart
@@ -4,11 +4,11 @@ class ErrorOr<T> {
   final T? value;
   final Object? error;
 
-  const ErrorOr({this.value, this.error});
+  const ErrorOr._({this.value, this.error});
 
-  const ErrorOr.value(T value) : this(value: value);
+  const ErrorOr.value(T value) : this._(value: value);
 
-  const ErrorOr.error(Object error) : this(error: error);
+  const ErrorOr.error(Object error) : this._(error: error);
 
   bool get hasValue => value != null;
 
