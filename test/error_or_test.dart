4375b681c1965d8380c2195ccea3dde6addcab02
Erlend Lind Madsen <erlendf80@gmail.com>
2022-04-03 20:34:08 +0200

    test: make ErrorOr instances const

--- test/error_or_test.dart
+++ test/error_or_test.dart
@@ -1,20 +1,29 @@
 import 'package:error_or/error_or.dart';
 import 'package:test/test.dart';
 
+class MyException implements Exception {
+  final String message;
+
+  const MyException(this.message);
+
+  @override
+  String toString() => message;
+}
+
 void main() {
   group('ErrorOr tests', () {
     test('Add Success with value 1', () {
-      final errorOr = ErrorOr.withValue(1);
+      const errorOr = ErrorOr.withValue(1);
       expect(errorOr.hasValue, true);
       expect(errorOr.hasError, false);
       expect(errorOr.value, 1);
     });
 
     test('Add Failure with Exception', () {
-      final errorOr = ErrorOr.withError(Exception('Error'));
+      const errorOr = ErrorOr.withError(MyException('Error'));
       expect(errorOr.hasValue, false);
       expect(errorOr.hasError, true);
-      expect(errorOr.error, isA<Exception>());
+      expect(errorOr.error, isA<MyException>());
       expect(errorOr.error.toString(), 'Exception: Error');
     });
   });
