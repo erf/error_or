2eb4b1ad8f54b25de4fc6529d5faa0ec8a610d4d
Erlend Lind Madsen <erlendf80@gmail.com>
2022-04-03 17:53:09 +0200

    change from Flutter to pure Dart package

--- test/error_or_test.dart
+++ test/error_or_test.dart
@@ -1,22 +1,23 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:error_or/error_or.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('add value 1 and expect 1', () {
-    const errorOr = ErrorOr.value(1);
-    expect(errorOr.hasValue, true);
-    expect(errorOr.hasError, false);
-    expect(errorOr.value, 1);
-    expect(errorOr.error, null);
-  });
+  group('ErrorOr tests', () {
+    test('add value 1 and expect 1', () {
+      const errorOr = ErrorOr.value(1);
+      expect(errorOr.hasValue, true);
+      expect(errorOr.hasError, false);
+      expect(errorOr.value, 1);
+      expect(errorOr.error, null);
+    });
 
-  test('add error and expect it', () {
-    final errorOr = ErrorOr.error(Exception('Error'));
-    expect(errorOr.hasValue, false);
-    expect(errorOr.hasError, true);
-    expect(errorOr.value, null);
-    expect(errorOr.error, isA<Exception>());
-    expect(errorOr.error.toString(), 'Exception: Error');
+    test('add error and expect it', () {
+      final errorOr = ErrorOr.error(Exception('Error'));
+      expect(errorOr.hasValue, false);
+      expect(errorOr.hasError, true);
+      expect(errorOr.value, null);
+      expect(errorOr.error, isA<Exception>());
+      expect(errorOr.error.toString(), 'Exception: Error');
+    });
   });
 }
