97734c5b2cfdacafa5026e1156c05a4b3bd75b34
Erlend Lind Madsen <erlendf80@gmail.com>
2022-04-03 17:51:59 +0200

    move error_or to lib/src

--- lib/error_or.dart
+++ lib/error_or.dart
@@ -1,16 +1,3 @@
 library error_or;
 
-class ErrorOr<T> {
-  final T? value;
-  final Object? error;
-
-  const ErrorOr._({this.value, this.error});
-
-  const ErrorOr.value(T value) : this._(value: value);
-
-  const ErrorOr.error(Object error) : this._(error: error);
-
-  bool get hasValue => value != null;
-
-  bool get hasError => error != null;
-}
+export 'src/error_or.dart';
