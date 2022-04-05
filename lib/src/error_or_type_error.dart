f6fc7a700ce4a709496d8800e51a87498225a5f5
Erlend Lind Madsen <erlendf80@gmail.com>
2022-04-05 18:49:58 +0200

    error_or_type_error: doc comments

--- lib/src/error_or_type_error.dart
+++ lib/src/error_or_type_error.dart
@@ -1,7 +1,10 @@
-/// Thrown if trying to access value or error if wrong type.
+/// Thrown if trying to access `error` or `value` and the [ErrorOr] is not of
+/// the correct type.
 class ErrorOrTypeError extends TypeError {
+  /// The error [message].
   final String message;
 
+  /// Creates a new [ErrorOrTypeError] with the given [message].
   ErrorOrTypeError(this.message) : super();
 
   @override
