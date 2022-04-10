38c4ed362d43b69006c14c249911f3ee5be73dc3
Erlend Lind Madsen <erlendf80@gmail.com>
2022-04-11 00:30:23 +0200

    example: add normal error handling

--- example/error_or_example.dart
+++ example/error_or_example.dart
@@ -18,20 +18,49 @@ Future<ErrorOr<String>> getValueOrError() async {
   }
 }
 
+Future<String> getValueOrThrows() async {
+  await Future.delayed(Duration(milliseconds: 500));
+  return maybeThrows();
+}
+
 void main() async {
   // ErrorOr example
+  print('* ErrorOr example');
   final errorOr = await getValueOrError();
   if (errorOr.hasError) {
     print(errorOr.error);
   } else {
     print(errorOr.value);
   }
+  print('');
 
   // ErrorOrTypeError example
+  print('* ErrorOrTypeError example');
   try {
     final errorOr = await getValueOrError();
     print(errorOr.value);
   } on ErrorOrTypeError catch (e) {
     print(e.message);
   }
+  print('');
+
+  // Normal Error handling with await and try/catch
+  print('* Normal Error handling with await and try/catch');
+  try {
+    final value = await getValueOrThrows();
+    print(value);
+  } on Exception catch (e) {
+    print(e);
+  }
+  print('');
+
+  // Normal Error handling with then and catchError
+  print('* Normal Error handling with then and catchError');
+  getValueOrThrows().then((value) {
+    print(value);
+  }).catchError((e) {
+    print(e);
+  }).whenComplete(() {
+    print('');
+  });
 }
